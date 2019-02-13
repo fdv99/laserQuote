78742680b76e53a5e47841f21e9f0fc6d19e89e9
Forrest Vaughan <forrest@v6carolina.com>
2019-02-13 16:57:12 -0500

    small changes, nothing works

--- laserQuote.py
+++ laserQuote.py
@@ -4,8 +4,22 @@ import sqlite3
 
 conn = sqlite3.connect('laserdata.db')
 c = conn.cursor()
-conn.close()
-carbonTable = 'Carbon_Table'
-stainlessTable = 'Stainless_Table'
 
-c.execute('CREATE TABLE carbonTable (thickness TEXT, cut_speed INTEGER, pierce_time INTEGER, cost_sq_foot REAL)')
+carbonTable = """
+CREATE TABLE CarbonTable (
+    thickness TEXT,
+    cut_speed INTEGER,
+    pierce_time INTEGER,
+    cost_sq_foot REAL)"""
+
+stainlessTable = """
+CREATE TABLE StainlessTable (
+    thickness TEXT,
+    cut_speed INTEGER,
+    pierce_time INTEGER,
+    cost_sq_foot REAL)"""
+try:
+    c.execute(carbonTable)
+    c.execute(stainlessTable)
+except:
+    print('Table already exists')
