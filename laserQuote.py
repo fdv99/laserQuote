5b0bec9143086ead377d72af497f30c1f481213d
Forrest Vaughan <forrest@v6carolina.com>
2019-02-15 13:34:39 -0500

    added variables

--- laserQuote.py
+++ laserQuote.py
@@ -1,25 +1,35 @@
-import sqlite3
 
 # This program will replace my excel spreadsheet of the same purpose, storing quotes in a database or an excel file
 
-conn = sqlite3.connect('laserdata.db')
-c = conn.cursor()
+# Variables
+materialType = 0
+materialThickness = 0
+length = 0 
+pierce = 0
+cutSpeed = 0
+pierceTime = 0
+handleTime = 20
+sheets = 1
+engineerTime = 20
+shopRate = 65
+engineerRate = 85
+laserRate = 105
+sheetHeight = 60
+sheetWidth = 120
+sheetUnitCost = 0
+materialCost = 0
 
-carbonTable = """
-CREATE TABLE CarbonTable (
-    thickness TEXT,
-    cut_speed INTEGER,
-    pierce_time INTEGER,
-    cost_sq_foot REAL)"""
+def material(materialType, materialThickness):
+    sheetUnitCost = 1 #go to correct table based on materialType, and get the correct cell on materialThickness row
+    cutSpeed = 10
+    pierceTime = 5
+    return cutSpeed, pierceTime
 
-stainlessTable = """
-CREATE TABLE StainlessTable (
-    thickness TEXT,
-    cut_speed INTEGER,
-    pierce_time INTEGER,
-    cost_sq_foot REAL)"""
-try:
-    c.execute(carbonTable)
-    c.execute(stainlessTable)
-except:
-    print('Table already exists')
+def laserTime(length, pierce, cutSpeed, pierceTime):
+    cutTime = (length / cutSpeed) + ((pierce * pierceTime) / 60)
+
+print(cutSpeed)
+
+material(1, 1)
+
+print(cutSpeed)
