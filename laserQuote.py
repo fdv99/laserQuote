cf459d5fa582c6ce35d32136ad27c1e4161c206c
Forrest Vaughan <forrest@v6carolina.com>
2019-03-15 15:10:02 -0400

    one table with carbon and stainless in it.  Can retrieve data now

--- laserQuote.py
+++ laserQuote.py
@@ -4,29 +4,19 @@ import csv
 import pandas as pd
 # This program will replace my excel spreadsheet of the same purpose, storing quotes in a database or an excel file
 
-def loadStainlessDB():
-    # This loads the cut data into the stainless table, no need to call again
-    df = pd.read_csv('stainless_cutdata.csv')
-    df.columns = df.columns.str.strip()
-    con = sqlite3.connect('cutdata.db')
-    df.to_sql("stainless", con)
-    con.close()
-
-def loadCarbonDB():
+def loadCutDataDB():
     # This loads the cut data into the carbon table, no need to call again
-    df = pd.read_csv('carbon_cutdata.csv')
+    df = pd.read_csv('cutdata.csv')
     df.columns = df.columns.str.strip()
     con = sqlite3.connect('cutdata.db')
-    df.to_sql("carbon", con)
+    df.to_sql("cutTable", con)
     con.close()
 
+# loadCutDataDB()
 
 def laserDB():
     con = sqlite3.connect('cutdata.db')
 
-
-carbonSizes = ['0.035 [20GA]', '0.047 [18GA]', '0.060 [16GA]', '0.075 [14GA]', '0.105 [12GA]', '0.120 [11GA]', '0.187 [HRPO]', '0.250 [HRPO]', '0.375 [HRPO]', '0.500 [HRPO]']
-stainlessSizes = ['0.035 [20GA]', '0.047 [18GA]', '0.060 [16GA]', '0.075 [14GA]', '0.120 [11GA]', '0.187 [3/16]']
 # Variables
 """
 materialType = 0
@@ -49,20 +39,24 @@ materialCost = 0
 
 
 def quote():
-    materialType = input("Material, carbon or stainless:")
-    if materialType == 'carbon':
-        materialThickness = '0.120 [11GA]'
-        # display carbonSizes list and pick size needed
-    elif materialType == 'stainless':
-        # display stainlessSizes list and pick size needed
-        materialThickness = '0.120 [11GA]'
+    materialType = input("Material, c or s: ")
+    if materialType == 'c':
+        materialThickness = 0.120
+    elif materialType == 's':
+        materialThickness = 0.120
     else:
         print("Not a valid material type.")
     con = sqlite3.connect('cutdata.db')
     cursor = con.cursor()
-    cursor.execute('SELECT CUT_SPEED FROM ? WHERE STEEL == ?', (materialType, materialThickness,))
-    cutSpeed = cursor.fetchone()
+    cursor.execute('SELECT * FROM cutTable WHERE steel = ? AND type = ?', (materialThickness, materialType))
+    data = cursor.fetchone()
+    cutSpeed = data[3]
+    pierceTime = data[4]
+    sheetUnitCost = data[7]
+    con.close()
     print(cutSpeed)
+    print(pierceTime)
+    print(sheetUnitCost)
 
 def laserTime(length, pierce, cutSpeed, pierceTime):
     cutTime = (length / cutSpeed) + ((pierce * pierceTime) / 60)
