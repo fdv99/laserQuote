ee8804e8d78c76ca0a5e15ca5d3625df05009023
Forrest Vaughan <forrest@v6carolina.com>
2019-08-26 16:44:58 -0400

    added frames

--- laserQuote.py
+++ laserQuote.py
@@ -6,8 +6,14 @@ import tkinter as tk
 # This program will replace my excel spreadsheet of the same purpose, storing quotes in a database or an excel file
 
 root = tk.Tk()
+topFrame = tk.Frame(root)
+topFrame.pack()
+bottomFrame = tk.Frame(root)
+bottomFrame.pack(side = "bottom")
+rightFrame = tk.Frame(root)
+rightFrame.pack(side ="right", fill="y")
 root.title("V6 Laser Quote")
-label = tk.Label(root, text = "Quote for Laser Cutting").grid(row=0,column=0)
+
 def loadCutDataDB():
     # This loads the cut data into the carbon table, no need to call again
     df = pd.read_csv('cutdata.csv')
@@ -63,87 +69,87 @@ def quote():
     print(f"Total Cost: {totalCost}")
 
 
-materialTypeLabel = tk.Label(root, text="Material Type [c,s]:", anchor="e").grid(row=1)
-materialTypeEntry = tk.Entry(root)
+materialTypeLabel = tk.Label(topFrame, text="Material Type [c,s]:", anchor="e").grid(row=1)
+materialTypeEntry = tk.Entry(topFrame)
 materialTypeEntry.grid(row=1,column=1)
 
 thickness = ["0.035", "0.047", "0.060", "0.075", "0.105", "0.120", "0.187", "0.250", "0.375", "0.500"]
 thickVariable = tk.StringVar(root)
 thickVariable.set(thickness[5])
-materialThickLabel = tk.Label(root, text="Material Thickness:", anchor="e").grid(row=2)
-materialThickOption = tk.OptionMenu(root, thickVariable, *thickness)
+materialThickLabel = tk.Label(topFrame, text="Material Thickness:", anchor="e").grid(row=2)
+materialThickOption = tk.OptionMenu(topFrame, thickVariable, *thickness)
 materialThickOption.grid(row=2, column=1)
 
-lengthLabel = tk.Label(root, text="Cut Length:", anchor="e").grid(row=3)
-lengthEntry = tk.Entry(root)
+lengthLabel = tk.Label(topFrame, text="Cut Length:", anchor="e").grid(row=3)
+lengthEntry = tk.Entry(topFrame)
 lengthEntry.grid(row=3,column=1)
 
-pierceLabel = tk.Label(root, text="Pierces:", anchor="e").grid(row=4)
-pierceEntry = tk.Entry(root)
+pierceLabel = tk.Label(topFrame, text="Pierces:", anchor="e").grid(row=4)
+pierceEntry = tk.Entry(topFrame)
 pierceEntry.grid(row=4,column=1)
 
-sheetLengthLabel = tk.Label(root, text="Sheet Length:", anchor="e").grid(row=5)
-sheetLengthEntry = tk.Entry(root)
+sheetLengthLabel = tk.Label(topFrame, text="Sheet Length:", anchor="e").grid(row=5)
+sheetLengthEntry = tk.Entry(topFrame)
 sheetLengthEntry.grid(row=5,column=1)
 
-sheetWidthLabel = tk.Label(root, text="Sheet Width:", anchor="e").grid(row=6)
-sheetWidthEntry = tk.Entry(root)
+sheetWidthLabel = tk.Label(topFrame, text="Sheet Width:", anchor="e").grid(row=6)
+sheetWidthEntry = tk.Entry(topFrame)
 sheetWidthEntry.grid(row=6,column=1)
 
-formTimeLabel = tk.Label(root, text="Forming Time:", anchor="e").grid(row=7)
-formTimeEntry = tk.Entry(root)
+formTimeLabel = tk.Label(topFrame, text="Forming Time:", anchor="e").grid(row=7)
+formTimeEntry = tk.Entry(topFrame)
 formTimeEntry.grid(row=7,column=1)
 
-sheetNumberLabel = tk.Label(root, text="Number of sheets:", anchor="e").grid(row=8)
-sheetNumberEntry = tk.Entry(root)
+sheetNumberLabel = tk.Label(topFrame, text="Number of sheets:", anchor="e").grid(row=8)
+sheetNumberEntry = tk.Entry(topFrame)
 sheetNumberEntry.grid(row=8,column=1)
 
 #Calculated Entries for costs and time:
-cutTimeLabel = tk.Label(root, text="Cutting Time:", anchor="e").grid(row=10)
-cutTimeEntry = tk.Entry(root)
-cutTimeEntry.grid(row=10,column=1)
+cutTimeLabel = tk.Label(bottomFrame, text="Cutting Time:", anchor="e").grid(row=0)
+cutTimeEntry = tk.Entry(bottomFrame)
+cutTimeEntry.grid(row=0,column=1)
 
-matCostLabel = tk.Label(root, text="Material Cost:", anchor="e").grid(row=11)
-matCostEntry = tk.Entry(root)
-matCostEntry.grid(row=11, column=1)
+matCostLabel = tk.Label(bottomFrame, text="Material Cost:", anchor="e").grid(row=1)
+matCostEntry = tk.Entry(bottomFrame)
+matCostEntry.grid(row=1, column=1)
 
-laserCostLabel = tk.Label(root, text="Laser Cost:", anchor="e").grid(row=12)
-laserCostEntry = tk.Entry(root)
-laserCostEntry.grid(row=12, column=1)
+laserCostLabel = tk.Label(bottomFrame, text="Laser Cost:", anchor="e").grid(row=2)
+laserCostEntry = tk.Entry(bottomFrame)
+laserCostEntry.grid(row=2, column=1)
 
 totalCost = 0
-totalCostLabel = tk.Label(root, text="Total Cost:", anchor="e").grid(row=13)
-totalCostEntry = tk.Entry(root)
-totalCostEntry.grid(row=13, column=1)
+totalCostLabel = tk.Label(bottomFrame, text="Total Cost:", anchor="e").grid(row=3)
+totalCostEntry = tk.Entry(bottomFrame)
+totalCostEntry.grid(row=3, column=1)
 
 #Default values for shop rates that can be changed:
-laserRateLabel = tk.Label(root, text="Laser Rate:", anchor="e").grid(row=1, column=3)
-laserRateEntry = tk.Entry(root)
+laserRateLabel = tk.Label(rightFrame, text="Laser Rate:", anchor="e").grid(row=1, column=3)
+laserRateEntry = tk.Entry(rightFrame)
 laserRateEntry.grid(row=1, column=4)
 laserRateEntry.insert(0, '105')
 
-handleRateLabel = tk.Label(root, text="Handling Rate:", anchor="e").grid(row=2, column=3)
-handleRateEntry = tk.Entry(root)
+handleRateLabel = tk.Label(rightFrame, text="Handling Rate:", anchor="e").grid(row=2, column=3)
+handleRateEntry = tk.Entry(rightFrame)
 handleRateEntry.grid(row=2, column=4)
 handleRateEntry.insert(0, '65')
 
-handleTimeLabel = tk.Label(root, text="Handling Time:", anchor="e").grid(row=3, column=3)
-handleTimeEntry = tk.Entry(root)
+handleTimeLabel = tk.Label(rightFrame, text="Handling Time:", anchor="e").grid(row=3, column=3)
+handleTimeEntry = tk.Entry(rightFrame)
 handleTimeEntry.grid(row=3, column=4)
 handleTimeEntry.insert(0, '25')
 
-engineerRateLabel = tk.Label(root, text="Engineering Rate:", anchor="e").grid(row=4, column=3)
-engineerRateEntry = tk.Entry(root)
+engineerRateLabel = tk.Label(rightFrame, text="Engineering Rate:", anchor="e").grid(row=4, column=3)
+engineerRateEntry = tk.Entry(rightFrame)
 engineerRateEntry.grid(row=4, column=4)
 engineerRateEntry.insert(0, '95')
 
-engineerTimeLabel = tk.Label(root, text="Engineering Time:", anchor="e").grid(row=5, column=3)
-engineerTimeEntry = tk.Entry(root)
+engineerTimeLabel = tk.Label(rightFrame, text="Engineering Time:", anchor="e").grid(row=5, column=3)
+engineerTimeEntry = tk.Entry(rightFrame)
 engineerTimeEntry.grid(row=5, column=4)
 engineerTimeEntry.insert(0, '20')
   
-calculateBut = tk.Button(root, text="Calculate", command=quote).grid(row=9,column=1)
+calculateBut = tk.Button(topFrame, text="Calculate", command=quote).grid(row=9,column=1)
 
-updateBut = tk.Button(root, text="Update", command=quote).grid(row=6,column=4)
+updateBut = tk.Button(rightFrame, text="Update", command=quote).grid(row=6,column=4)
 
 root.mainloop()
